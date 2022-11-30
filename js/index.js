63062e123618bb1cff4c0b507a76656e0b1a4424
Тигай <ktigay@action-media.ru>
2022-11-30 10:18:52 +0300

    День 4 рефакторинг

--- js/index.js
+++ js/index.js
@@ -1,138 +1,124 @@
-const API_URL = 'http://localhost:3024/';
+{
+	const API_URL = 'http://localhost:3024/';
 
-let dataMusic = [];
-let playlist = [];
+	let dataMusic = [];
+	let playlist = [];
 
-const favoriteList = localStorage.getItem('favorite')
-	? JSON.parse(localStorage.getItem('favorite'))
-	: [];
+	const favoriteList = localStorage.getItem('favorite')
+		? JSON.parse(localStorage.getItem('favorite'))
+		: [];
 
-const audio = new Audio();
-const headerLogo = document.querySelector('.header__logo');
-const favoriteBtn = document.querySelector('.header__favorite-btn');
-const tracksCard = document.getElementsByClassName('track');
-const catalogContainer = document.querySelector('.catalog__container');
-const player = document.querySelector('.player');
-const pauseBtn = document.querySelector('.player__controller-pause');
-const stopBtn = document.querySelector('.player__controller-stop');
-const prevBtn = document.querySelector('.player__controller-prev');
-const nextBtn = document.querySelector('.player__controller-next');
-const likeBtn = document.querySelector('.player__controller-like');
-const muteBtn = document.querySelector('.player__icon-mute');
-const playerProgressInput = document.querySelector('.player__progress-input');
-const trackTitle = document.querySelector('.track-info__title');
-const trackArtist = document.querySelector('.track-info__artist');
+	const audio = new Audio();
 
-const playerTimePassed = document.querySelector('.player__time-passed');
-const playerTimeTotal = document.querySelector('.player__time-total');
-const playerVolumeInput = document.querySelector('.player__volume-input');
+	const headerLogo = document.querySelector('.header__logo');
+	const search = document.querySelector('.search');
+	const favoriteBtn = document.querySelector('.header__favorite-btn');
 
-const search = document.querySelector('.search');
+	const tracksCard = document.getElementsByClassName('track');
+	const catalogContainer = document.querySelector('.catalog__container');
 
-const catalogAddBtn = document.createElement('button');
-catalogAddBtn.classList.add('catalog__btn-add');
-catalogAddBtn.innerHTML = `
-	<span>Увидеть все</span>
-	<svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-		<path d="M8.58997 16.59L13.17 12L8.58997 7.41L9.99997 6L16 12L9.99997 18L8.58997 16.59Z"/>
-	</svg>
-`;
+	const player = document.querySelector('.player');
+	const trackTitle = document.querySelector('.track-info__title');
+	const trackArtist = document.querySelector('.track-info__artist');
+	const pauseBtn = document.querySelector('.player__controller-pause');
+	const stopBtn = document.querySelector('.player__controller-stop');
+	const prevBtn = document.querySelector('.player__controller-prev');
+	const nextBtn = document.querySelector('.player__controller-next');
+	const likeBtn = document.querySelector('.player__controller-like');
+	const muteBtn = document.querySelector('.player__icon-mute');
+	const playerProgressInput = document.querySelector('.player__progress-input');
+	const playerTimePassed = document.querySelector('.player__time-passed');
+	const playerTimeTotal = document.querySelector('.player__time-total');
+	const playerVolumeInput = document.querySelector('.player__volume-input');
 
-const pausePlayer = () => {
-	const trackActive = document.querySelector('.track_active');
+	const pausePlayer = () => {
+		const trackActive = document.querySelector('.track_active');
 
-	if (audio.paused) {
-		audio.play().then(() => {
-			pauseBtn.classList.remove('player__icon_play');
-			trackActive.classList.remove('track_pause');
-		});
-	} else {
-		audio.pause();
-		pauseBtn.classList.add('player__icon_play');
-		trackActive.classList.add('track_pause');
-	}
-}
-
-const playMusic = (event) => {
-	event.preventDefault();
-	const trackActive = event.currentTarget;
-
-	if (trackActive.classList.contains('track_active')) {
-		pausePlayer();
-		return;
-	}
-
-	let i = 0;
-	const id = trackActive.dataset.idTrack;
-
-	const index = favoriteList.indexOf(id);
-	if (index !== -1) {
-		likeBtn.classList.add('player__icon_like_active');
-	} else {
-		likeBtn.classList.remove('player__icon_like_active');
-	}
-
-	const track = playlist.find((item, index) => {
-		i = index;
-		return id === item.id;
-	});
-	audio.src = `${API_URL}${track.mp3}`;
-	trackTitle.textContent = track.track;
-	trackArtist.textContent = track.artist;
-
-	audio.pause();
-
-	setTimeout(() => {
-		audio.play().then(() => {
-			pauseBtn.classList.remove('player__icon_play');
-			player.classList.add('player_active');
-			player.dataset.idTrack = id;
-		});
-	}, 150);
-
-	const prevTrack = i === 0 ? playlist.length - 1 : i - 1;
-	const nextTrack = i + 1 === playlist.length ? 0 : i + 1;
-	prevBtn.dataset.idTrack = playlist[prevTrack].id;
-	nextBtn.dataset.idTrack = playlist[nextTrack].id;
-	likeBtn.dataset.idTrack = id;
-
-	for (let i = 0; i < tracksCard.length; i++) {
-		if (id === tracksCard[i].dataset.idTrack) {
-			tracksCard[i].classList.add('track_active');
-		} else {
-			tracksCard[i].classList.remove('track_active');
-		}
-	}
-};
-
-const addHandlerTrack = () => {
-	for (let i = 0; i < tracksCard.length; i++) {
-		tracksCard[i].addEventListener('click', playMusic);
-	}
-}
-
-pauseBtn.addEventListener('click', pausePlayer);
-
-stopBtn.addEventListener('click', () => {
-	audio.src = '';
-	player.classList.remove('player_active');
-	document.querySelector('.track_active').classList.remove('track_active');
-});
-
-const createCard = (data) => {
-
-	const card = document.createElement('a');
-	card.href = '#';
-	card.classList.add('catalog__item', 'track');
-	if (player.dataset.idTrack === data.id) {
-		card.classList.add('track_active');
 		if (audio.paused) {
-			card.classList.add('track_pause');
+			audio.play().then(() => {
+				pauseBtn.classList.remove('player__icon_play');
+				trackActive.classList.remove('track_pause');
+			});
+		} else {
+			audio.pause();
+			pauseBtn.classList.add('player__icon_play');
+			trackActive.classList.add('track_pause');
 		}
 	}
-	card.dataset.idTrack = data.id;
 
-	card.innerHTML = `
+	const playMusic = (event) => {
+		event.preventDefault();
+		const trackActive = event.currentTarget;
+
+		if (trackActive.classList.contains('track_active')) {
+			pausePlayer();
+			return;
+		}
+
+		let i = 0;
+		const id = trackActive.dataset.idTrack;
+		const index = favoriteList.indexOf(id);
+
+		if (index !== -1) {
+			likeBtn.classList.add('player__icon_like_active');
+		} else {
+			likeBtn.classList.remove('player__icon_like_active');
+		}
+
+		const track = playlist.find((item, index) => {
+			i = index;
+			return id === item.id;
+		});
+
+		audio.src = `${API_URL}${track.mp3}`;
+		trackTitle.textContent = track.track;
+		trackArtist.textContent = track.artist;
+
+		audio.pause();
+
+		setTimeout(() => {
+			audio.play().then(() => {
+				pauseBtn.classList.remove('player__icon_play');
+				player.classList.add('player_active');
+				player.dataset.idTrack = id;
+			});
+		}, 150);
+
+		const prevTrack = i === 0 ? playlist.length - 1 : i - 1;
+		const nextTrack = i + 1 === playlist.length ? 0 : i + 1;
+		prevBtn.dataset.idTrack = playlist[prevTrack].id;
+		nextBtn.dataset.idTrack = playlist[nextTrack].id;
+		likeBtn.dataset.idTrack = id;
+
+		for (let i = 0; i < tracksCard.length; i++) {
+			if (id === tracksCard[i].dataset.idTrack) {
+				tracksCard[i].classList.add('track_active');
+			} else {
+				tracksCard[i].classList.remove('track_active');
+			}
+		}
+	};
+
+	const addHandlerTrack = () => {
+		for (let i = 0; i < tracksCard.length; i++) {
+			tracksCard[i].addEventListener('click', playMusic);
+		}
+	}
+
+	const createCard = (data) => {
+		const card = document.createElement('a');
+		card.href = '#';
+		card.classList.add('catalog__item', 'track');
+
+		if (player.dataset.idTrack === data.id) {
+			card.classList.add('track_active');
+			if (audio.paused) {
+				card.classList.add('track_pause');
+			}
+		}
+
+		card.dataset.idTrack = data.id;
+		card.innerHTML = `
 		 <div class="track__img-wrap">
 			 <img src="${API_URL}${data.poster}"
 					alt="${data.artist} ${data.track}"
@@ -147,124 +133,155 @@ const createCard = (data) => {
 		 </div>
 	`;
 
-	return card;
-}
-
-const renderCatalog = (dataList) => {
-	playlist = [...dataList];
-	catalogContainer.textContent = '';
-	const listCards = dataList.map(createCard);
-	catalogContainer.append(...listCards);
-	addHandlerTrack();
-}
-
-const checkCount = (i = 1) => {
-	if (catalogContainer.clientHeight + 20 > tracksCard[0].clientHeight * 3) {
-		tracksCard[tracksCard.length - i].style.display = 'none';
-		checkCount(i + 1);
-	} else if (i !== 1) {
-		catalogContainer.append(catalogAddBtn);
+		return card;
 	}
-}
 
-const updateTime = () => {
-	const duration = audio.duration;
-	const currentTime = audio.currentTime;
-	const progress = (currentTime / duration) * playerProgressInput.max;
-	playerProgressInput.value = progress ? progress : 0;
+	const renderCatalog = (dataList) => {
+		playlist = [...dataList];
+		if (!playlist.length) {
+			catalogContainer.textContent = 'Ничего не найдено';
+			return;
+		}
+		catalogContainer.textContent = '';
 
-	const minutesPassed = Math.floor(currentTime / 60) || '0';
-	const secondsPassed = Math.floor(currentTime % 60) || '0';
+		const listCards = dataList.map(createCard);
+		catalogContainer.append(...listCards);
+		addHandlerTrack();
 
-	const minutesDuration = Math.floor(duration / 60) || '0';
-	const secondsDuration = Math.floor(duration % 60) || '0';
+		const catalogAddBtn = createCatalogBtn();
+		checkCount(catalogAddBtn);
+	}
 
-	playerTimePassed.textContent = `
-		${minutesPassed}:${secondsPassed < 10 ? '0' + secondsPassed : secondsPassed}`;
-	playerTimeTotal.textContent = `
-		${minutesDuration}:${secondsDuration < 10 ? '0' + secondsDuration : secondsDuration}`;
-}
+	const checkCount = (catalogAddBtn, i = 1) => {
+		if (catalogContainer.clientHeight + 20 > tracksCard[0].clientHeight * 3) {
+			tracksCard[tracksCard.length - i].style.display = 'none';
+			return checkCount(catalogAddBtn, i + 1);
+		}
 
-const init = async () => {
-	audio.volume = localStorage.getItem('volume') || 1;
-	playerVolumeInput.value = audio.volume * 100;
+		if (i !== 1) {
+			catalogContainer.append(catalogAddBtn);
+		}
+	}
 
-	dataMusic = await fetch(`${API_URL}api/music`).then((data) => data.json());
+	const updateTime = () => {
+		const duration = audio.duration;
+		const currentTime = audio.currentTime;
+		const progress = (currentTime / duration) * playerProgressInput.max;
 
-	renderCatalog(dataMusic);
-	checkCount();
+		playerProgressInput.value = progress ? progress : 0;
 
-	catalogAddBtn.addEventListener('click', () => {
-		[...tracksCard].forEach((trackCard) => {
-			trackCard.style.display = '';
-			catalogAddBtn.remove();
+		const minutesPassed = Math.floor(currentTime / 60) || '0';
+		const secondsPassed = Math.floor(currentTime % 60) || '0';
+
+		const minutesDuration = Math.floor(duration / 60) || '0';
+		const secondsDuration = Math.floor(duration % 60) || '0';
+
+		playerTimePassed.textContent = `
+			${minutesPassed}:${secondsPassed < 10 ? '0' + secondsPassed : secondsPassed}`;
+		playerTimeTotal.textContent = `
+			${minutesDuration}:${secondsDuration < 10 ? '0' + secondsDuration : secondsDuration}`;
+	}
+
+	const createCatalogBtn = () => {
+		const catalogAddBtn = document.createElement('button');
+		catalogAddBtn.classList.add('catalog__btn-add');
+		catalogAddBtn.innerHTML = `
+			<span>Увидеть все</span>
+			<svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+				<path d="M8.58997 16.59L13.17 12L8.58997 7.41L9.99997 6L16 12L9.99997 18L8.58997 16.59Z"/>
+			</svg>
+		`;
+
+		catalogAddBtn.addEventListener('click', () => {
+			[...tracksCard].forEach((trackCard) => {
+				trackCard.style.display = '';
+				catalogAddBtn.remove();
+			});
+		})
+
+		return catalogAddBtn;
+	}
+
+	const addEventListener = () => {
+		prevBtn.addEventListener('click', playMusic);
+		nextBtn.addEventListener('click', playMusic);
+
+		audio.addEventListener('ended', () => {
+			nextBtn.dispatchEvent(new Event('click', {bubbles: true}))
+		})
+
+		audio.addEventListener('timeupdate', updateTime);
+
+		playerProgressInput.addEventListener('change', () => {
+			const progress = playerProgressInput.value;
+			audio.currentTime = (progress / playerProgressInput.max) * audio.duration;
+		})
+
+		favoriteBtn.addEventListener('click', () => {
+			const data = dataMusic.filter((item) => favoriteList.includes(item.id));
+			renderCatalog(data);
+		})
+
+		headerLogo.addEventListener('click', () => {
+			renderCatalog(dataMusic);
+		})
+
+		likeBtn.addEventListener('click', () => {
+			const index = favoriteList.indexOf(likeBtn.dataset.idTrack);
+			if (index === -1) {
+				favoriteList.push(likeBtn.dataset.idTrack);
+				likeBtn.classList.add('player__icon_like_active');
+			} else {
+				favoriteList.splice(index, 1);
+				likeBtn.classList.remove('player__icon_like_active');
+			}
+
+			localStorage.setItem('favorite', JSON.stringify(favoriteList));
+		})
+
+		playerVolumeInput.addEventListener('input', () => {
+			const value = playerVolumeInput.value;
+			audio.volume = value / 100;
+		})
+
+		muteBtn.addEventListener('click', () => {
+			if (audio.volume) {
+				localStorage.setItem('volume', audio.volume);
+				audio.volume = 0;
+				muteBtn.classList.add('player__icon-mute-off');
+				playerVolumeInput.value = 0;
+			} else {
+				audio.volume = localStorage.getItem('volume');
+				muteBtn.classList.remove('player__icon-mute-off');
+				playerVolumeInput.value = audio.volume * 100;
+			}
+		})
+
+		pauseBtn.addEventListener('click', pausePlayer);
+
+		stopBtn.addEventListener('click', () => {
+			audio.src = '';
+			player.classList.remove('player_active');
+			document.querySelector('.track_active').classList.remove('track_active');
 		});
-	})
 
-	prevBtn.addEventListener('click', playMusic);
-	nextBtn.addEventListener('click', playMusic);
+		search.addEventListener('submit', (event) => {
+			event.preventDefault();
+			fetch(`${API_URL}api/music?search=${search.search.value}`)
+				.then((data) => data.json())
+				.then(renderCatalog);
+		});
+	}
 
-	audio.addEventListener('ended', () => {
-		nextBtn.dispatchEvent(new Event('click', {bubbles: true}))
-	})
+	const init = () => {
+		audio.volume = localStorage.getItem('volume') || 1;
+		playerVolumeInput.value = audio.volume * 100;
 
-	audio.addEventListener('timeupdate', updateTime);
+		fetch(`${API_URL}api/music`)
+			.then((data) => data.json())
+			.then(renderCatalog)
+			.finally(addEventListener);
+	}
 
-	playerProgressInput.addEventListener('change', () => {
-		const progress = playerProgressInput.value;
-		audio.currentTime = (progress / playerProgressInput.max) * audio.duration;
-	})
-
-	favoriteBtn.addEventListener('click', () => {
-		const data = dataMusic.filter((item) => favoriteList.includes(item.id));
-		renderCatalog(data);
-		checkCount();
-	})
-
-	headerLogo.addEventListener('click', () => {
-		renderCatalog(dataMusic);
-		checkCount();
-	})
-
-	likeBtn.addEventListener('click', () => {
-		const index = favoriteList.indexOf(likeBtn.dataset.idTrack);
-		if (index === -1) {
-			favoriteList.push(likeBtn.dataset.idTrack);
-			likeBtn.classList.add('player__icon_like_active');
-		} else {
-			favoriteList.splice(index, 1);
-			likeBtn.classList.remove('player__icon_like_active');
-		}
-
-		localStorage.setItem('favorite', JSON.stringify(favoriteList));
-	})
-
-	playerVolumeInput.addEventListener('input', () => {
-		const value = playerVolumeInput.value;
-		audio.volume = value / 100;
-	})
-
-	muteBtn.addEventListener('click', () => {
-		if (audio.volume) {
-			localStorage.setItem('volume', audio.volume);
-			audio.volume = 0;
-			muteBtn.classList.add('player__icon-mute-off');
-			playerVolumeInput.value = 0;
-		} else {
-			audio.volume = localStorage.getItem('volume');
-			muteBtn.classList.remove('player__icon-mute-off');
-			playerVolumeInput.value = audio.volume * 100;
-		}
-	})
-
-	search.addEventListener('submit', async (event) => {
-		event.preventDefault();
-
-		playlist = await fetch(`${API_URL}api/music?search=${search.search.value}`).then((data) => data.json());
-
-		renderCatalog(playlist);
-		checkCount();
-	});
+	init();
 }
-
-init();
